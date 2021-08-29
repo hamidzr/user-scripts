9af629b8edd60d8f8c8ddc188a7edc5ee14c1635
Hamid Zare <hamidzare.vu@gmail.com>
2021-08-29 15:39:26 -0700

    add csv export for personal capital holdings

---
+++ raw/personal-capital.js
@@ -0,0 +1,35 @@
+hmd = hmd || {};
+if (hmd.download === undefined) {
+  hmd.download = (filename, text) => {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  };
+}
+
+const getRows = () => {
+  document.querySelectorAll('#holdings .pc-holdings-grid-cell--holding-description').forEach(el => {
+    el.remove();
+  })
+
+  let rows = Array.from(document.querySelectorAll('#holdings .table__body.qa-datagrid-rows .table__row '));
+
+  rows = rows.map(row => Array.from(row.querySelectorAll('.qa-holding-shares, .qa-ticker')))
+
+  return rows.map(row => row.map(el => el.textContent).join(', '));
+};
+
+function main() {
+  const text =  getRows().join('\n');
+  // use the loaded browser-mods from hamidzr/user-script to download the resulting csv
+  hmd.download('holdings.csv', text);
+}
+
+main();
